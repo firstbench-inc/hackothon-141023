ec142368689340dd51d81eab1263137b64adef2a
MBUYT0N <shusrith03@gmail.com>
2023-10-14 11:21:12 +0530

    navs

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,13 +1,68 @@
-import React, {useEffect, useState, useContext} from "react";
-import Image from 'next/image'
-import Link from 'next/link'
+import React, { useEffect, useState, useContext } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-import Style from './NavBar.module.css'
-import {AppContext} from '../../Context/appContext'
-import {Model, Error} from '../index'
- 
+import Style from "./NavBar.module.css";
+import { AppContext } from "../../Context/appContext";
+import { Model, Error } from "../index";
+import images from "../../assets";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const menuItems = [
+    {
+      menu: "All Users",
+      link: "/",
+    },
+    {
+      menu: "Chat",
+      link: "/",
+    },
+    {
+      menu: "Contact",
+      link: "/",
+    },
+    {
+      menu: "Settings",
+      link: "/",
+    },
+    {
+      menu: "All Users",
+      link: "/",
+    },
+  ];
+
+  const [Active, setActive] = useState(2);
+  const [open, setOpen] = useState(false);
+  const [openModel, setOpenModel] = useState(false);
+  const [account, userName, connectWallet] = useContext(AppContext);
+
+  return (
+    <div className={Style.NavBar}>
+      NavBar
+      <div className={Style.NavBar_box}>
+        <div className={Style.NavBar_box_left}>
+          <img src={images.logo} alt="logo" width={50} height={50} />{" "}
+          {/* Use 'img' instead of 'Image' */}
+        </div>
+        <div className={Style.NavBar_box_right}>
+          <div className={Style.NavBar_box_right_menu}>
+            {menuItems.map((el, i) => (
+              <div
+                onClick={() => setActive(i + 1)}
+                key={i + 1}
+                className={`${Style.NavBar_box_right_menu_items} ${
+                  active === i + 1 ? Style.active_btn : ""
+                }`}
+              >
+                <Link className={Style.NavBar_box_right_menu_items_link} href={el.link}>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
