1a297026c0c6a1125e6c90c63d3d80f47bdddd2c
MBUYT0N <shusrith03@gmail.com>
2023-10-14 10:32:18 +0530

    assets

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, {useEffect, useState, useContext} from "react";
+import Image from 'next/image'
+import Link from 'next/link'
 
+import Style from './NavBar.module.css'
+import {AppContext} from '../../Context/appContext'
+import {Model, Error} from '../index'
+ 
 const NavBar = () => {
   return <div>NavBar</div>;
 };
